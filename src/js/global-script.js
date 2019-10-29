9bd837ae8b6e7e8ed917c459f0190422ba3515cf
Popeskul Pavel <peterjj91@gmail.com>
2019-10-29 23:19:01 +0200

    view full slider

--- src/js/global-script.js
+++ src/js/global-script.js
@@ -13,6 +13,31 @@ $(document).ready(function() {
 });
 
 // $(document).ready(function() {
+//   $('.slider-full').slick({
+//     dots: false,
+//     slidesToShow: 1,
+//     slidesToScroll: 1,
+//   });
+// });
+
+$(document).ready(function() {
+  $('.slider-full-for').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    fade: true,
+    asNavFor: '.slider-full-nav'
+  });
+  $('.slider-full-nav').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: true,
+    asNavFor: '.slider-full-for',
+    focusOnSelect: true
+  });
+});
+
+// $(document).ready(function() {
 //   $('.multiple-items').slick({
 //     infinite: false,
 //     dots: true,
